1c9b86a2a12e823d064186bb390244242f0a4578
Galactic President Superstar Mcawesomeville <alwichma@microsoft.com>
2020-09-30 13:07:59 +0200

    initial commit

---
+++ Visualbean.Pokemon/Controllers/PokemonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Visualbean.Pokemon.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        private readonly ILogger<PokemonController> _logger;
+
+        public PokemonController(ILogger<PokemonController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
