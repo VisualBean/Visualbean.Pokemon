e3ce34b8621b1e90bb562242781bcd9a7d4c5c5e
Galactic President Superstar Mcawesomeville <alwichma@microsoft.com>
2020-10-01 11:23:22 +0200

    move docker file and remove https redirect.

--- Visualbean.Pokemon/Startup.cs
+++ Visualbean.Pokemon/Startup.cs
@@ -49,8 +49,6 @@ namespace Visualbean.Pokemon
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
